e7934ac0a84d3e150c268ab5c4d265d90e003c0e
Siarhei Mahlysh <maglish@mail.ru>
2018-07-27 14:35:50 +0300

    Initial commit

---
+++ StudentsApp.Server/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentsApp.Shared.Models;
+
+namespace StudentsApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        // GET: api/Student
+        [HttpGet]
+        [Route("Students")]
+        public IEnumerable<Student> Get()
+        {
+            return new List<Student>{ new Student {Name = "Siarhei", Age = 23, Faculty = "Computer tecnology" }};
+        }
+    }
+}
---
+++ StudentsApp.Shared/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentsApp.Shared.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Faculty { get; set; }
+    }
+}
